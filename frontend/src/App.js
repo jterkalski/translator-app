fb3cb4773ee10740ba50419d92ddbcb398e0b892
Kuba Terkalski <jakub.terkalski@gmail.com>
2023-02-20 22:11:31 +0100

    added proxy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
 const App = () => {
-    return <div>App</div>;
+    const [text, setText] = useState('');
+    const textareaRef = useRef();
+
+    const handleChange = () => {
+        setText(textareaRef.current.value);
+    };
+
+    return (
+        <textarea
+            ref={textareaRef}
+            onChange={handleChange}
+            placeholder='Aa'
+            style={{ padding: '0.5rem' }}
+        />
+    );
 };
 
 export default App;
